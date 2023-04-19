8a3c932ee09ac48747ce74d4ae6db311f8669fc3
Peter Lucido <petermlucido@gmail.com>
2023-04-19 11:02:58 -0500

    set up taco cat

---
+++ routes/tacos.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as tacosCtrl from '../controllers/tacos.js'
+import { isLoggedIn } from '../middleware/middleware.js'
+
+
+const router = Router()
+
+router.get('/', tacosCtrl.index)
+router.post('/', tacosCtrl.create)
+router.post('/', isLoggedIn, tacosCtrl.create)
+
+export {
+  router
+}
